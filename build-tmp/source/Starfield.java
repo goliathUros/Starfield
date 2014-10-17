22a06baf5107c253267e0dbfd31dec864c330d16
unknown <dayi@697-334-Shelob.sf.k12.internal>
2014-10-17 13:25:39 -0700

    Loading onto github page

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] test = new Particle[100];
 
-void setup()
+public void setup()
 {
   background(0);
   size(400,400);
@@ -11,7 +27,7 @@ void setup()
   }
 }
 
-void draw()
+public void draw()
 {
   background(0);
   for (int i = 0; i < test.length; i++) 
@@ -24,7 +40,7 @@ void draw()
 class NormalParticle implements Particle
 {
   double xpos, ypos, speed, angle, partSize;
-  color partFill;
+  int partFill;
   NormalParticle() 
   {
     xpos = width/2;
@@ -51,7 +67,7 @@ class NormalParticle implements Particle
     {
       xpos = width/2;
       ypos = height/2;
-      speed = speed + 0.02;
+      speed = speed + 0.02f;
       partSize = partSize - 1;
     }
   }
@@ -66,7 +82,7 @@ interface Particle
 class OddballParticle implements Particle
 {
   double xpos, ypos, speed, angle;
-  color partFill;
+  int partFill;
   OddballParticle() 
   {
     xpos = width/2;
@@ -105,3 +121,12 @@ class OddballParticle implements Particle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
